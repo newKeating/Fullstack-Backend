76e6d885f4213de42815b2680df14b0033b1a264
Peter Kim <kroits09@gmail.com>
2020-12-21 21:47:00 +0900

    Add fieldResolver for textSnippet of Post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -2,11 +2,13 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   InputType,
   Int,
   Mutation,
   Query,
   Resolver,
+  Root,
   UseMiddleware,
 } from "type-graphql";
 import { getConnection } from "typeorm";
@@ -23,8 +25,15 @@ class PostInput {
   text: string;
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(
+    @Root() post: Post // Injecting Post object
+  ) {
+    return post.text.slice(0, 50);
+  }
+
   @Query(() => [Post])
   // posts(@Ctx() ctx: MyContext): Promise<Post[]> {
   posts(
